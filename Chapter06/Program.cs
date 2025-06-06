88c9a13ecc2e179984006ee87db7b6446a23ad95
Marko Nedzbala <marko.nedzbala@gmail.com>
2025-06-06 15:41:18 -0400

    EOD 2025-06-06

---
+++ Chapter06/Program.cs
@@ -0,0 +1,66 @@
+﻿
+Car myCar = new Car(80) { Speed = 50 };
+
+Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Car
+{
+    public readonly int MaxSpeed;
+    private int _currSpeed;
+
+    public Car(int max)
+    {
+        MaxSpeed = max;
+    }
+
+    public Car()
+    {
+        MaxSpeed = 55;
+    }
+
+    public int Speed
+    {
+        get { return _currSpeed; }
+        set
+        {
+            _currSpeed = value;
+            if (_currSpeed > MaxSpeed)
+            {
+                _currSpeed = MaxSpeed;
+            }
+        }
+    }
+}
+
+
+
+
