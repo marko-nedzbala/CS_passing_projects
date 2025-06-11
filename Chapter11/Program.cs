eb6afe7963d4c0b0767ec251e32d95a4314c4ba0
Marko Nedzbala <marko.nedzbala@gmail.com>
2025-06-11 16:33:37 -0400

    EOD 2025-06-11

--- Chapter11/Program.cs
+++ Chapter11/Program.cs
@@ -1,4 +1,23 @@
-﻿
+﻿using System.Collections;
+using Chapter11;
+
+List<Person> myPeople = new List<Person>();
+myPeople.Add(new Person("Bob", "Smith", 45));
+myPeople.Add(new Person("Jimmy", "Johns", 32));
+
+myPeople[0] = new Person("Sue", "Test", 35);
+
+foreach (Person person in myPeople)
+{
+    Console.WriteLine("Person: {0}", person);
+}
+
+Console.WriteLine();
+
+Point p1 = new Point(100, 100);
+Point p2 = new Point(40, 55);
+Console.WriteLine("Add {0}", p1 + p2);
+Console.WriteLine("Subtract {0}", p1 - p2);
 
 
 
@@ -34,11 +53,16 @@
 
 
 
+public class PersonCollection
+{
+    private List<Person> people = new List<Person>();
 
-
-
-
-
+    public Person this[int index]
+    {
+        get => (Person)people[index];
+        set => people.Insert(index, value);
+    }
+}
 
 
 
